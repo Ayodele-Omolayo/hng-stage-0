07494bc4a8c8710d3480e5e85966ba54f543cd7b
Ayodele Omolayo <ayodemolayo@gmail.com>
2024-06-27 19:06:32 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const nameElement = document.getElementById("name");
+  const usernameElement = document.getElementById("username");
+
+  nameElement.textContent = "Ayodele Omolayo";
+  usernameElement.textContent = "ladyOps";
+});
